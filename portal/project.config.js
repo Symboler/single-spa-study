bd2b00054834976651d90e8822b6bf7604f45cdc
l1449 <luxl@dtdream.com>
2019-06-23 21:57:54 +0800

    构建dist文件夹

---
+++ portal/project.config.js
@@ -0,0 +1,13 @@
+module.exports={projects:[{
+    "name": "reactApp",
+    "path": ["/reactApp"],
+    "prefix": "/reactApp/",
+    "main": "/reactApp/main.js",
+    "store": "/reactApp/store.js"
+  },{
+    "name": "vueApp",
+    "path": ["/vueApp"],
+    "prefix": "/vueApp/",
+    "main": "/vueApp/main.js",
+    "store": "/vueApp/store.js"
+  }]}
